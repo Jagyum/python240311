10cc2c489d8a8b956a0a36908fb84ef6846558be
Jagyum <jagyum@gmail.com>
2024-03-12 10:36:39 +0900

    클래스 연습 완료

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,14 @@
+# 전역변수
 strName = "Not Class Member"
 
 class DemoString:
     def __init__(self):
+        # 인스턴스 멤버변수
         self.strName = "" 
     def set(self, msg):
         self.strName = msg
     def print(self):
-        print(str)
+        print(self.strName)
 
 d = DemoString()
 d.set("First Message")
